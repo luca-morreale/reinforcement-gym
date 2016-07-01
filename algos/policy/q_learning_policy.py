f21e409411bf51cedff8353c943e9ded712ac990
Luca Morreale <luca.morreale93@gmail.com>
2016-07-02 00:38:08 +0200

    Created QLearning policy

---
+++ algos/policy/q_learning_policy.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from policy.policy import Policy
+from action_chooser.variation_epsilon_chooser import VariationEpsilonGreedyChooser
+from numpy import max
+
+
+class QLearningPolicy(Policy):
+
+    def __init__(self, Q, updater, m, randomEpisodes):
+        self.randomEpisodes = randomEpisodes
+        self.actionChooser = VariationEpsilonGreedyChooser(1, m)
+        self.Q = Q
+        self.updater = updater
+        self.newEpisode()
+
+    def doEpisode(self, episode_n):
+        observation = self.env.reset()
+
+        self.nextState = observation
+        step = 0
+
+        while True:
+            last_state = self.nextState
+            action = self.getAction(last_state)
+            step += 1
+
+            if self.show:
+                self.env.render()
+
+            observation, reward, done, info = self.env.step(action)
+
+            self.appendToHistory(last_state, action, reward)
+            self.nextState = observation
+
+            #self.Q.prettyPrintQ()
+            if done or step > self.env.spec.timestep_limit:
+                print(('finished episode', episode_n, 'steps', step))
+                break
+            self.update(last_state, action, reward)
+        self.newEpisode()
+
+    def newEpisode(self):
+        super().newEpisode()
+        if self.randomEpisodes > 0:
+            self.randomEpisodes -= 1
+            self.actionChooser.epsilon = 1
+        else:
+            self.actionChooser.epsilon = 0
+
+    def estimateDelta(self, value, reward, gamma):
+        maxQ = max(self.Q.getPossibleActions(self.nextState))
+        return reward + gamma * maxQ - value
