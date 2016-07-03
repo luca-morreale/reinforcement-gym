e12821ecde99e82ff747c89039a444072fcfda0f
Luca Morreale <luca.morreale93@gmail.com>
2016-07-03 23:35:47 +0200

    Fixed Norm and Hash generalizer

--- algos/generalizer/hash_state_generalizer.py
+++ algos/generalizer/hash_state_generalizer.py
@@ -1,25 +1,52 @@
 # -*- coding: utf-8 -*-
 from generalizer.state_generalizer import StateGeneralizer
+from state_action import StateAction
+import numpy as np
 
 
 class HashGeneralizer(StateGeneralizer):
 
-    def __init__(self, updater, cellSize):
-        super().__init__(updater)
-        self.cellSize = cellSize
+    """Create a generalizer which use spatial hashing.
 
-    def getQState(self, state):
-        h = self._hashState(state)
+    Args:
+        cellSize:     size of each cell
+    """
+    def __init__(self, m, cellSize, obs_space):
+        super().__init__(m)
+        self.cellSize = cellSize
+        self.size = len(obs_space.high)
+        self.rndseq = np.random.randint(0, 2 ** 32 - 1, self.size + 1)
+
+    def getRepresentation(self, state_action):
+        h = self._hashState(state_action)
         if h in self.Q:
             return h
-        self.Q[h] = []
+        self.Q[h] = 0
         return h
 
-    def _hashState(self, s):
-        primes = [73856093, 19349663, 83492791, 67867979]
+    def getQValue(self, state_action):
+        index = self.getRepresentation(state_action)
+        return self.Q[index]
+
+    def _hashState(self, state_action):
         h = 0
-        for i in range(len(s.obs)):
-            h += (s.obs[i] / self.cellSize) * primes[i]
+        for i in range(self.size):
+            h += (state_action.obs[i] / self.cellSize) * self.rndseq[i]
+        h += state_action.action * self.rndseq[self.size]
         return int(h)
-        # hash = (int(pos.x / cellSize) * 73856093) ^ (int(pos.y / cellSize)
-        # * 19349663) ^ (int(pos.z / cellSize) * 83492791);
+
+    """ Update the value of a state-action pair adding the given value.
+    Args:
+        state_action:    object representing the state-action
+        value:           value to add to the current value
+    """
+    def addDeltaToQValue(self, state_action, value):
+        if isinstance(state_action, StateAction):
+            index = self.getRepresentation(state_action)
+        else:
+            index = state_action
+        self.Q[index] += value
+
+    def prettyPrintQ(self):
+        for key, value in enumerate(self.Q):
+            print((str(key) + "-> ", value))
--- algos/generalizer/norm_state_generalizer.py
+++ algos/generalizer/norm_state_generalizer.py
@@ -1,20 +1,48 @@
 # -*- coding: utf-8 -*-
 from scipy.spatial.distance import euclidean
 from generalizer.state_generalizer import StateGeneralizer
+from state_action import StateAction
 
 
 class NormGeneralizer(StateGeneralizer):
 
-    def __init__(self, updater, cellSize):
-        super().__init__(updater)
+    """Create a generalizer which use spatial hashing.
+
+    Args:
+        cellSize:     size of each cell
+    """
+    def __init__(self, m, cellSize):
+        super().__init__(m)
         self.cellSize = cellSize
 
-    def getQState(self, state):
+    def getRepresentation(self, state_action):
         for s in self.Q:
-            if self.isNear(s, state):
+            if self.isNear(s, state_action):
                 return s
-        self.Q[state] = []
-        return state
+        self.Q[state_action] = 0
+        return state_action
+
+    def getQValue(self, state_action):
+        index = self.getRepresentation(state_action)
+        return self.Q[index]
 
     def isNear(self, o1, o2):
-        return euclidean(o1.obs, o2.obs) < self.cellSize
+        if o1.action == o2.action:
+            return euclidean(o1.obs, o2.obs) < self.cellSize
+        return False
+
+    """ Update the value of a state-action pair adding the given value.
+    Args:
+        state_action:    object representing the state-action
+        value:           value to add to the current value
+    """
+    def addDeltaToQValue(self, state_action, value):
+        if isinstance(state_action, StateAction):
+            index = self.getRepresentation(state_action)
+        else:
+            return
+        self.Q[index] += value
+
+    def prettyPrintQ(self):
+        for key, value in enumerate(self.Q):
+            print((str(key) + "-> ", value))
