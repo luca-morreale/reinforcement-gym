d4ae671e786979170af288f7929b4068ddccbfb9
Luca Morreale <luca.morreale93@gmail.com>
2016-07-14 00:08:20 +0200

    Added hash and equal function to StateAction

--- algos/state_action.py
+++ algos/state_action.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from copy import copy
+import numpy as np
 
 
 class StateAction():
@@ -9,7 +10,17 @@ class StateAction():
         self.action = copy(action)
         self.value = value
 
+    def __eq__(self, other):
+        if not isinstance(other, StateAction):
+            return False
+        return np.array_equal(other.obs, self.obs) \
+                    and other.action == self.action
+
+    def __hash__(self):
+        return hash(frozenset(self.obs)) + \
+                        31 * hash(self.action)
+
     def __str__(self):
         return "StateAction[obs: " + str(self.obs) + \
-                    "action: " + str(self.id) + \
+                    "action: " + str(self.action) + \
                     " , value: " + str(self.value) + "]"
