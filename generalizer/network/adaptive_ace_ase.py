203de80e336d100806179e37b389e238c1f2fbbb
Luca Morreale <luca.morreale93@gmail.com>
2016-11-24 20:36:26 +0100

    Moved instruction in class methods

--- generalizer/network/adaptive_ace_ase.py
+++ generalizer/network/adaptive_ace_ase.py
@@ -8,21 +8,29 @@ import numpy as np
 # of Barto, Sutton and Anderson
 
 class ACEASE():
-    def __init__(self, session, state_dim, action_dim, gamma=0.8, learning_rate=0.1):
+    def __init__(self, session, state_dim, gamma=0.8, learning_rate=0.1):
         self.critic = ACE(session, state_dim, gamma, learning_rate)
-        self.actor = ASE(session, state_dim, action_dim, learning_rate)
+        self.actor = ASE(session, state_dim, learning_rate)
         self._state_dim = state_dim
-        self._action_dim = action_dim
         self._session = session
         self._session.run(tf.initialize_all_variables())
 
     def train(self, state, reward):
         delta = self.critic.get_internal_signal(np.reshape(state, self._state_shape()), reward)
-        self.actor.train(np.reshape(state, self._state_shape()), delta)
-        self.critic.train(np.reshape(state, self._state_shape()), delta)
+        state = np.reshape(state, self._state_shape())
+        self._train_networks(state, actor_target_out=delta, critic_target_out=reward)
+
+    def _train_networks(self, state, actor_target_out, critic_target_out):
+        self.actor.train(state, actor_target_out)
+        self.critic.train(state, critic_target_out)
 
     def _state_shape(self):
         return tuple(reversed( (self._state_dim, ) + (1,) ))
 
     def get_action(self, state):
         return self.actor.predict(np.reshape(state, self._state_shape()))
+
+    def transform_into_batch(self, state_batch, length):
+        state_shape = list(self._state_shape())
+        state_shape[0] = length
+        return np.reshape(state_batch, tuple(state_shape))
